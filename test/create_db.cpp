6d287a3a5bf65b3e1eb037ed14b14a219e5115a0
Jalen Adams <leftysolara@gmail.com>
2015-10-16 23:42:31 -0400

    remove catch tests before moving to Qt unit tests

--- test/create_db.cpp
+++
@@ -1,9 +0,0 @@
-#include "catch.hpp"
-#include "../include/database.h"
-
-TEST_CASE ("database connections can be created", "[database, creation]") {
-
-  Database *test_db = new Database("employee_test.db");
-  REQUIRE( test_db->error_check() == false );
-  delete test_db;
-}
