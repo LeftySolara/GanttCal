00494f8404718508f29fcf85d06f3e9bd762a5fb
Jalen Adams <leftysolara@gmail.com>
2015-10-16 23:40:59 -0400

    merge sql scripts into one

--- scripts/db_init.sql
+++ scripts/db_init.sql
@@ -2,16 +2,25 @@ PRAGMA foreign_keys = ON;
 
 CREATE TABLE employees (
     employee_id INT NOT NULL PRIMARY KEY,
-    first_name VARCHAR(45) NOT NULL,
     last_name VARCHAR(45) NOT NULL,
+    first_name VARCHAR(45) NOT NULL,
     max_hours SMALLINT NULL
     schedule_color VARCHAR(7) NULL
     );
 
+-- Create table for possible days and populate it
 CREATE TABLE weekdays (
     weekday_id SMALLINT NOT NULL PRIMARY KEY,
     name VARCHAR(10) NOT NULL UNIQUE
     );
+
+INSERT INTO weekdays VALUES (0, "Sunday");
+INSERT INTO weekdays VALUES (1, "Monday");
+INSERT INTO weekdays VALUES (2, "Tuesday");
+INSERT INTO weekdays VALUES (3, "Wednesday");
+INSERT INTO weekdays VALUES (4, "Thursday");
+INSERT INTO weekdays VALUES (5, "Friday");
+INSERT INTO weekdays VALUES (6, "Saturday");
 
 CREATE TABLE shifts (
     shift_id INT NOT NULL PRIMARY KEY,
@@ -23,3 +32,5 @@ CREATE TABLE shifts (
     FOREIGN KEY (workday_id) REFERENCES weekdays(weekday_id),
     FOREIGN KEY (employee_id) REFERENCES employees(employee_id)
     );
+
+COMMIT
