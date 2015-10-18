22fb6ea5342f628f7dd46e94beea11303accbaf7
Jalen Adams <leftysolara@gmail.com>
2015-10-17 22:04:24 -0400

    change INSERT statements to check for existing values

--- scripts/db_init.sql
+++ scripts/db_init.sql
@@ -1,28 +1,48 @@
 PRAGMA foreign_keys = ON;
 
-CREATE TABLE employees (
+CREATE TABLE IF NOT EXISTS employees (
     employee_id INT NOT NULL PRIMARY KEY,
     last_name VARCHAR(45) NOT NULL,
     first_name VARCHAR(45) NOT NULL,
-    max_hours SMALLINT NULL
+    max_hours SMALLINT NULL,
     schedule_color VARCHAR(7) NULL
     );
 
 -- Create table for possible days and populate it
-CREATE TABLE weekdays (
+CREATE TABLE IF NOT EXISTS weekdays (
     weekday_id SMALLINT NOT NULL PRIMARY KEY,
     name VARCHAR(10) NOT NULL UNIQUE
     );
 
-INSERT INTO weekdays VALUES (0, "Sunday");
-INSERT INTO weekdays VALUES (1, "Monday");
-INSERT INTO weekdays VALUES (2, "Tuesday");
-INSERT INTO weekdays VALUES (3, "Wednesday");
-INSERT INTO weekdays VALUES (4, "Thursday");
-INSERT INTO weekdays VALUES (5, "Friday");
-INSERT INTO weekdays VALUES (6, "Saturday");
+INSERT INTO weekdays(weekday_id,name)
+SELECT 0, 'Sunday'
+WHERE NOT EXISTS(SELECT 1 FROM weekdays WHERE weekday_id = 0 AND name = 'Sunday');
 
-CREATE TABLE shifts (
+INSERT INTO weekdays(weekday_id,name)
+SELECT 1, 'Monday'
+WHERE NOT EXISTS(SELECT 1 FROM weekdays WHERE weekday_id = 1 AND name = 'Monday');
+
+INSERT INTO weekdays(weekday_id,name)
+SELECT 2, 'Tuesday'
+WHERE NOT EXISTS(SELECT 1 FROM weekdays WHERE weekday_id = 2 AND name = 'Tuesday');
+
+INSERT INTO weekdays(weekday_id,name)
+SELECT 3, 'Wednesday'
+WHERE NOT EXISTS(SELECT 1 FROM weekdays WHERE weekday_id = 3 AND name = 'Wednesday');
+
+INSERT INTO weekdays(weekday_id,name)
+SELECT 4, 'Thursday'
+WHERE NOT EXISTS(SELECT 1 FROM weekdays WHERE weekday_id = 4 AND name = 'Thursday');
+
+INSERT INTO weekdays(weekday_id,name)
+SELECT 5, 'Friday'
+WHERE NOT EXISTS(SELECT 1 FROM weekdays WHERE weekday_id = 5 AND name = 'Friday');
+
+INSERT INTO weekdays(weekday_id,name)
+SELECT 6, 'Saturday'
+WHERE NOT EXISTS(SELECT 1 FROM weekdays WHERE weekday_id = 6 AND name = 'Saturday');
+
+CREATE TABLE IF NOT EXISTS shifts (
     shift_id INT NOT NULL PRIMARY KEY,
     workday_id SMALLINT NOT NULL,
     start_time TIME NOT NULL,
@@ -33,4 +53,3 @@ CREATE TABLE shifts (
     FOREIGN KEY (employee_id) REFERENCES employees(employee_id)
     );
 
-COMMIT
